c0b18b8ee2203ee554e9d73dbdac64e1c4997b8c
wuji-lucky <1216563237@qq.com>
2023-10-16 15:45:08 +0800

    项目内容补充

--- testDemo-biz/main/java/com/example/demo/biz/utils/RedisUtil.java
+++ testDemo-biz/main/java/com/example/demo/biz/utils/RedisUtil.java
@@ -13,14 +13,18 @@ public class RedisUtil {
   // RedisTemplate是Spring Data Redis提供的一个高级封装类，它允许我们通过一些方便的方法来操作Redis。
   @Autowired private RedisTemplate<String, String> redisTemplate;
 
-  // 存储数据到Redis,并且不设置过期时间
+  /*
+   * 存储数据到Redis,并且不设置过期时间
+   */
   public void set(String key, String value) {
     // ValueOperations是RedisTemplate中的一个接口，它提供了各种对Redis中的值进行操作的方法
     ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
     valueOperations.set(key, value);
   }
 
-  // 存储数据到Redis,并且设置了过期时间
+  /*
+   * 存储数据到Redis,并且设置了过期时间
+   */
   public void set(String key, String value, int time) {
     ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
     valueOperations.set(key, value, time, TimeUnit.MINUTES);
@@ -28,7 +32,9 @@ public class RedisUtil {
     // 如:long b = TimeUnit.HOURS.convert(58L, TimeUnit.MINUTES);//将58分钟转换为小时
   }
 
-  // 通过key获取value
+  /*
+   * 通过key获取value
+   */
   public String get(String key) {
     ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
     if (redisTemplate.hasKey(key)) {
@@ -38,7 +44,9 @@ public class RedisUtil {
     }
   }
 
-  // 判断是否存在对应的key
+  /*
+   * 判断是否存在对应的key
+   */
   public boolean exist(String key) {
     if (this.get(key) != null) {
       return true;
@@ -47,8 +55,17 @@ public class RedisUtil {
     }
   }
 
-  // 删除key
+  /*
+   * 删除key
+   */
   public void delete(String key) {
     redisTemplate.delete(key);
+  }
+
+  /*
+   * 给key设置过期时间
+   */
+  public Boolean expire(String key, long timeout, TimeUnit unit) {
+    return redisTemplate.expire(key, timeout, unit);
   }
 }
